3f237064766aaab22535b146ae050800f18395c4
FerMV <munoz.valencia.fernando@gmail.com>
2024-10-23 20:43:03 -0600

    Tu mama

--- clase21-10-24/to-do.js
+++ clase21-10-24/to-do.js
@@ -1,2 +1,17 @@
-let arreglo = () => [];
-let crear = document.createElement(hacer);
+let task = ["comprar organos", "Estudiar", "golpear un bagabundo"];
+
+console.log("lista de tareas", task);
+
+task.push("cantar metal");
+
+task.splice(0,0);
+
+task.push("ir a casa")
+
+//------------- usando porEach ---------------------
+
+let tasks = ["comprar", "Estudiar", "limpiar casa", "ir a casa"]
+
+tasks.forEach((tasks, index)=>{
+    console.log(`Tarea ${index} ${tasks}`)
+})
